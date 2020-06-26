5b59a7e8d734b17d38ae1be881f1b25d2ea2fc57
softmarshmallow <woojoo@softmarshmallow.com>
2020-06-26 12:28:14 +0900

    ✅ python utils: dumped flutter_material_icon_specs.json

--- packages/python_utils/flutter_utils/material_icons_parser/material_icons_regex.py
+++ packages/python_utils/flutter_utils/material_icons_parser/material_icons_regex.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import json
 
 # "https://github.com/flutter/flutter/blob/master/packages/flutter/lib/src/material/icons.dart"
 flutter_material_icons_dart_source = "https://raw.githubusercontent.com/flutter/flutter/master/packages/flutter/lib/src/material/icons.dart"
@@ -19,12 +20,26 @@ icon_data_detector_token = "static const IconData "
 def parse():
     r = requests.get(flutter_material_icons_dart_source)
     source_text = r.text
+    spec = []
     for l in source_text.split("\n"):
         if icon_data_detector_token in l:
+            # >> IconData(code_point, font_family) from flutter's material icon
             icon_name = re.search(r'static const IconData (.*?) = IconData\(', l).group(1)
-            print(icon_name, l)
-    ...
+            icon_font_family = "MaterialIcons"  # this is static, no need to parse
+            icon_code_point_text = re.search(r'IconData\((.*?), fontFamily:', l).group(1)
+            icon_code_point = int(icon_code_point_text, 16)
+            print(icon_name, icon_code_point, icon_font_family)
+            spec.append({
+                "name": icon_name,
+                "code_point": icon_code_point,
+                "font_family": icon_font_family
+            })
+    return spec
 
 
+spec_file_dir = "flutter_material_icon_specs.json"
 if __name__ == "__main__":
-    parse()
+    spec = parse()
+    with open(spec_file_dir, "w") as f:
+        json.dump(spec, f, ensure_ascii=True, indent=4, sort_keys=True)
+    print(spec)
