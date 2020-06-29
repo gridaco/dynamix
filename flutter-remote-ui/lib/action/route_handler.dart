873387cb28a3ebd28336a6984d824655458a49ea
softmarshmallow <woojoo@softmarshmallow.com>
2020-06-29 16:38:51 +0900

    😀 added action handler

--- flutter-remote-ui/lib/action/route_handler.dart
+++ flutter-remote-ui/lib/action/route_handler.dart
@@ -1,6 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_remote_ui/action/action_handler.dart';
 
+class RouteHandler extends ActionHandler {
+  RouteHandler(BuildContext context, {ActionData actionData})
+      : super(context, actionData: actionData);
 
-class RouteHandler extends ActionHandler{
+  factory RouteHandler.of(BuildContext context, {ActionData actionData}) {
+    return RouteHandler(context, actionData: actionData);
+  }
 
+  @override
+  handle() {
+    Navigator.of(context).pushNamed(actionData.route);
+  }
 }
