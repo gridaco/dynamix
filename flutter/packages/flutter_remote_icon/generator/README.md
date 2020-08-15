bd8e49f19e61c2550040174cd36b6d6c035c9326
softmarshmallow <woojoo@softmarshmallow.com>
2020-08-15 23:53:22 +0900

    🥰 added source gen as separate package

---
+++ flutter/packages/flutter_remote_icon/generator/README.md
@@ -0,0 +1,1 @@
+## Icon mapping generator
--- flutter/packages/flutter_remote_icon/generator/lib/builder/builder.dart
+++ flutter/packages/flutter_remote_icon/generator/lib/builder/builder.dart
@@ -1,6 +1,6 @@
 import 'package:build/build.dart';
-import 'package:flutter_remote_icon/src/builder/generator.dart';
 import 'package:source_gen/source_gen.dart';
+import 'generator.dart';
 
 Builder iconMapper(BuilderOptions options) =>
     SharedPartBuilder([IconMapperGenerator()], 'icon_mapper');
