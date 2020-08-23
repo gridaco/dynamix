b130a25bb57cf284c4d8fa099ef528a29845d0a6
softmarshmallow <woojoo@softmarshmallow.com>
2020-08-24 02:54:55 +0900

    😀 initial commit

---
+++ flutter/packages/x_color/test/x_color_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:x_color/x_color.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}
