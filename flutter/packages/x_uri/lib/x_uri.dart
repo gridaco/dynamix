e780ca217049ffcf714b090274f3bc9f37c28d4a
softmarshmallow <woojoo@softmarshmallow.com>
2020-08-28 12:20:03 +0900

    🔥 added x_uri

--- flutter/packages/x_uri/lib/x_uri.dart
+++ flutter/packages/x_uri/lib/x_uri.dart
@@ -1,7 +1,4 @@
 library x_uri;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/action/action.dart';
+export 'src/resource.dart';
