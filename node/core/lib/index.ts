a6dc294ae1ffdf253e704a1c81d641b8dba972e7
softmarshmallow <woojoo@softmarshmallow.com>
2020-09-27 03:12:30 +0900

    exported color

--- node/core/lib/index.ts
+++ node/core/lib/index.ts
@@ -9,11 +9,13 @@ import { layouts } from "./layouts"
 import * as widgets from "./widgets"
 import * as icons from "./icons"
 import * as screens from "./screen-specs"
+import { Color } from "./colors";
 
 export {
     Actions as actions,
     widgets as widgets,
     widgets as ui,
+    Color,
     icons,
     layouts,
     screens
