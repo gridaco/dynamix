237581fa51126acad1cfe7876029d08611b3a6ae
Universe <universe@grida.co>
2023-12-29 19:23:26 +0900

    config

--- www/docusaurus.config.ts
+++ www/docusaurus.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://dynamix-www.vercel.app",
+  url: "https://grida.co",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/dynamix/",
-
+  organizationName: "gridaco",
+  projectName: "dynamix",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
