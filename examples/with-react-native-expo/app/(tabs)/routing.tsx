e83392aea52e358d50247781af65bb47cd2ea7a9
Universe <universe@grida.co>
2024-01-07 12:56:12 +0900

    tabs

---
+++ examples/with-react-native-expo/app/(tabs)/routing.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
